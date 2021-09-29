8690ff421ecd13678480e20b684d8eef9e09e320
Branden Newberg <bnewberg-cuellar@mail.csuchico.edu>
2021-09-28 22:07:55 -0700

    Generate my-services component and add content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IntroductionComponent } from './introduction/introduction.component';
+import { MyServicesComponent } from './my-services/my-services.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
-        IntroductionComponent
+        IntroductionComponent,
+        MyServicesComponent
     ],
     imports: [
         BrowserModule,
