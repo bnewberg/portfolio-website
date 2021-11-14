257df1895404af7b7578a10dd455f383c4f74688
Branden Newberg <bnewberg-cuellar@mail.csuchico.edu>
2021-11-13 16:54:34 -0800

    Add initial icons to footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faAngular, faDev, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
     selector: 'app-footer',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+    faGithub = faGithub;
+    faDev = faDev;
+    faTwitter = faTwitter;
+    faAngular = faAngular
+
 
     constructor() { }
 
