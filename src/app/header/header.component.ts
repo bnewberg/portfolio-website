c397268aa74b12f3205a34df102505c3272245ba
Branden Newberg <bnewberg-cuellar@mail.csuchico.edu>
2021-11-13 22:01:48 -0800

    Add logic for opening and closing nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+    isNavOpen = false;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    onNavToggle() {
+        this.isNavOpen = !this.isNavOpen;
+    }
+
+    getNavTransform() {
+        return this.isNavOpen ? "translateX(0%)" : "translateX(100%)";
+    }
+
+    getNavTogglePosition() {
+        return this.isNavOpen ? "fixed" : "absolute";
+    }
 
 }
