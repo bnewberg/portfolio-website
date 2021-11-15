45cce50a95f1b1dffba6cf9f68b58b8fad4f79c5
Branden Newberg <bnewberg-cuellar@mail.csuchico.edu>
2021-11-14 20:38:27 -0800

    Add hamburger animation on click

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -17,12 +17,4 @@ export class HeaderComponent implements OnInit {
         this.isNavOpen = !this.isNavOpen;
     }
 
-    getNavTransform() {
-        return this.isNavOpen ? "translateX(0%)" : "translateX(100%)";
-    }
-
-    getNavTogglePosition() {
-        return this.isNavOpen ? "fixed" : "absolute";
-    }
-
 }
